f16ff1765da650347347e405d6251854402d55a1
JSONIsaac <yeiyei19829@gmail.com>
2024-05-07 10:29:34 -0600

    add changes

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+    private cars = [
+        {
+            id: 1,
+            brand: 'Toyota',
+            model: 'Corolla',
+            year: 2015
+        },
+        {
+            id: 2,
+            brand: 'BMW',
+            model: 'X5',
+            year: 2018
+        },
+        {
+            id: 3,
+            brand: 'Ford',
+            model: 'Fiesta',
+            year: 2016
+        },
+        {
+            id: 4,
+            brand: 'Audi',
+            model: 'A4',
+            year: 2017
+        }
+    ];
+    
+    findAll() {
+        return this.cars;
+    }
+
+    findById(id: number) {
+        const car = this.cars.find(car => car.id === id);
+            if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
+
+        return car;
+    }
+}
